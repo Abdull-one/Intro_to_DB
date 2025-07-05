78a4207b99aad1b0c7fe066dc91abe005c1d6507
Abdullahi Ali Adam <abdullahijangade@gmail.com>
2025-07-05 23:59:59 +0100

    task_6.sql

---
+++ task_6.sql
@@ -0,0 +1,6 @@
+--  Insert multiple rows into the Customers table
+INSERT INTO Customers (customer_name, email, address)
+VALUES 
+('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+('Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
